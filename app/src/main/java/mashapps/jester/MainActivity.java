f0c90157eb1817fc12740a8fac1eddce8c6bd901
Michael Scofield <michaelscofswork@gmail.com>
2018-08-13 18:37:21 -0700

    Added Navpage and AppTheme

--- app/src/main/java/mashapps/jester/MainActivity.java
+++ app/src/main/java/mashapps/jester/MainActivity.java
@@ -1,7 +1,9 @@
 package mashapps.jester;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enterApp(View view) {
+        // Declaring intent to move to NavPage
+        Intent intent = new Intent(this, NavPage.class);
+        // Starting that intent
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/mashapps/jester/NavPage.java
@@ -0,0 +1,13 @@
+package mashapps.jester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_page);
+    }
+}
