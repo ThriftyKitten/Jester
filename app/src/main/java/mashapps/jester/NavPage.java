340c00fe76e87bfc3ca3bfe6294cdc13d83f0a5d
Michael Scofield <michaelscofswork@gmail.com>
2018-09-12 15:38:32 -0700

    Messed with Gridview for title art. Added expand page for onclick.

--- app/src/main/java/mashapps/jester/NavPage.java
+++ app/src/main/java/mashapps/jester/NavPage.java
@@ -1,7 +1,11 @@
 package mashapps.jester;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class NavPage extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class NavPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_page);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                enterExpand(v);
+            }
+        });
     }
+
+    public void enterExpand(View view) {
+        // Declaring intent to move to Expand page
+        Intent intent = new Intent(this, ExpandImage.class);
+        // Starting that intent
+        startActivity(intent);
+    }
+
 }
+
+
